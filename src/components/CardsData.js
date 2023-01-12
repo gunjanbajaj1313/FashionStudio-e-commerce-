3e4e2f25aabc1c3a6d44d5a8a29c49c1df28d8d0
gunjanbajaj1313 <113934167+gunjanbajaj1313@users.noreply.github.com>
2023-01-12 12:47:25 +0530

    first commit

---
+++ src/components/CardsData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "Men's T-shirt",
+        imgdata: "https://m.media-amazon.com/images/I/61-FeTqeaoL._UX569_.jpg",
+        address: "Fashion Market,Delhi",
+        delimg: "https://m.media-amazon.com/images/I/61-FeTqeaoL._UX569_.jpg",
+        somedata: " 870 + order placed from here recently",
+        price: 650,
+        rating: "3.5",
+        arrimg: "https://m.media-amazon.com/images/I/61-FeTqeaoL._UX569_.jpg",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: "Men's Suit",
+        imgdata: "https://5.imimg.com/data5/MB/PF/MY-3693506/designer-mens-suits-500x500.jpg",
+        address: "Arvind Store,Kanpur",
+        delimg: "https://5.imimg.com/data5/MB/PF/MY-3693506/designer-mens-suits-500x500.jpg",
+        somedata: " 2714 + order placed from here recently",
+        price: 3500,
+        rating: "3.9",
+        arrimg: "https://5.imimg.com/data5/MB/PF/MY-3693506/designer-mens-suits-500x500.jpg",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "Pink Saree",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnm8IrR1sgIpSKr8WRIbwBQ4zFFK5l3xnXZouJu3JmFysP34QWFhE82-Ft6ikVHMBTlKo&usqp=CAU",
+        address: "Delhi Plaza",
+        delimg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnm8IrR1sgIpSKr8WRIbwBQ4zFFK5l3xnXZouJu3JmFysP34QWFhE82-Ft6ikVHMBTlKo&usqp=CAU",
+        somedata: " 1269 + order placed from here recently",
+        price: 1700,
+        rating: "4.5",
+        arrimg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnm8IrR1sgIpSKr8WRIbwBQ4zFFK5l3xnXZouJu3JmFysP34QWFhE82-Ft6ikVHMBTlKo&usqp=CAU",
+         qnty:0
+    },
+    {
+        id: 4,
+        rname: "Wedding Gown",
+        imgdata: "https://m.media-amazon.com/images/I/31v2e2kFyBL.jpg",
+        address: "MumbaiFashion",
+        delimg: "https://m.media-amazon.com/images/I/31v2e2kFyBL.jpg",
+        somedata: " 1470 + order placed from here recently",
+        price: 13500,
+        rating: "4.6",
+        arrimg: "https://m.media-amazon.com/images/I/31v2e2kFyBL.jpg",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "Green Salwar-Suit",
+        imgdata: "https://assets0.mirraw.com/images/9173530/AS-8387-C_large_m.jpg?1624879300",
+        address: "Surat Market",
+        delimg: "https://assets0.mirraw.com/images/9173530/AS-8387-C_large_m.jpg?1624879300",
+        somedata: "785 + order placed from here recently",
+        price: 750,
+        rating: "3.8",
+        arrimg: "https://assets0.mirraw.com/images/9173530/AS-8387-C_large_m.jpg?1624879300",
+         qnty:0
+    },
+    {
+        id: 6,
+        rname: "Black One-Piece Dress",
+        imgdata: "https://images.meesho.com/images/products/162506957/cpag6_256.jpg",
+        address: "Delhi Plaza",
+        delimg: "https://images.meesho.com/images/products/162506957/cpag6_256.jpg",
+        somedata: " 1175 + order placed from here recently",
+        price: 850,
+        rating: "3.9",
+        arrimg: "https://images.meesho.com/images/products/162506957/cpag6_256.jpg",
+         qnty:0
+    },
+    {
+        id: 7,
+        rname: "Wedding Shervani",
+        imgdata: "https://www.parivarceremony.com/media/catalog/product/cache/14a1553305c404c221b3f13f50c007dd/m/z/mz3008m12.jpg",
+        address: "Surat Bazaar",
+        delimg: "https://www.parivarceremony.com/media/catalog/product/cache/14a1553305c404c221b3f13f50c007dd/m/z/mz3008m12.jpg",
+        somedata: "985 + order placed from here recently",
+        price: 19000,
+        rating: "4.7",
+        arrimg: "https://www.parivarceremony.com/media/catalog/product/cache/14a1553305c404c221b3f13f50c007dd/m/z/mz3008m12.jpg",
+         qnty:0
+    },
+    {
+        id: 8,
+        rname: "Men's Formal Shirt",
+        imgdata: "https://rukminim1.flixcart.com/image/612/612/ksuowi80/shirt/w/p/o/xxl-frml-st2-vebnor-original-imag6bkegkwmqv75.jpeg?q=70",
+        address: "Tea, Coffee, Shake, Beverages",
+        delimg: "https://rukminim1.flixcart.com/image/612/612/ksuowi80/shirt/w/p/o/xxl-frml-st2-vebnor-original-imag6bkegkwmqv75.jpeg?q=70",
+        somedata: "650 + order placed from here recently",
+        price: 800,
+        rating: "3.2",
+        arrimg: "https://rukminim1.flixcart.com/image/612/612/ksuowi80/shirt/w/p/o/xxl-frml-st2-vebnor-original-imag6bkegkwmqv75.jpeg?q=70",
+         qnty:0
+    },
+    {
+        id: 9,
+        rname: "Wedding Lehanga",
+        imgdata: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTolgIA4E8Tv4OWzBxxDLGjNkAor_GD9wywuQ&usqp=CAU",
+        address: "Mumbai Store",
+        delimg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTolgIA4E8Tv4OWzBxxDLGjNkAor_GD9wywuQ&usqp=CAU",
+        somedata: "4578 + order placed from here recently",
+        price: 35000,
+        rating: "4.9",
+        arrimg: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTolgIA4E8Tv4OWzBxxDLGjNkAor_GD9wywuQ&usqp=CAU",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
